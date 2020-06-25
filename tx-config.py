58cafa1a61a91a0f1e7fb1b11c36fcea41377b5f
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-06-25 15:33:41 +0200

    adding script for config generation, tx init

---
+++ tx-config.py
@@ -0,0 +1,30 @@
+import os
+
+project_slug = "nethermind-docs"
+
+with open(".tx/config", "a") as file:
+    file.write(f"[main]\n")
+    file.write(f"host = https://www.transifex.com\n")
+    file.write("\n")
+    
+search_path = '.' 
+root, dirs, files = next(os.walk(search_path), ([],[],[]))
+
+dirs = [directory for directory in dirs if not directory.startswith(".")]
+countFiles = 0
+
+for directory in dirs:
+    rootdir = directory
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            filePath = os.path.join(subdir, file).replace("/","-").replace(".", "-")
+            fileRaw = os.path.join(subdir, file)
+            countFiles += 1
+            with open(".tx/config", "a") as file:
+                file.write(f"[{project_slug}.{filePath}]\n")
+                file.write(f"file_filter = {subdir}/locales/<lang>\n")
+                file.write(f"source_file = {fileRaw}\n")
+                file.write("source_lang = en\n")
+                file.write("type = GITHUBMARKDOWN\n")
+                file.write("\n")
+print(f"Number of resources: {countFiles}")
