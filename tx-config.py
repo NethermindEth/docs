d3b1ed0b5fefd73a480ec34785cfc22abc06c7b7
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-06-25 16:08:40 +0200

    changing config/script

--- tx-config.py
+++ tx-config.py
@@ -13,18 +13,29 @@ root, dirs, files = next(os.walk(search_path), ([],[],[]))
 dirs = [directory for directory in dirs if not directory.startswith(".")]
 countFiles = 0
 
+with open(".tx/config", "a") as file:
+                file.write(f"[{project_slug}.README-md]\n")
+                file.write(f"file_filter = README.<lang>.md\n")
+                file.write(f"source_file = README.md\n")
+                file.write("source_lang = en\n")
+                file.write("type = GITHUBMARKDOWN\n")
+                file.write("\n")
+
 for directory in dirs:
     rootdir = directory
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             filePath = os.path.join(subdir, file).replace("/","-").replace(".", "-")
             fileRaw = os.path.join(subdir, file)
+            fileName = os.path.basename(fileRaw)
+            fileNameNoExtension = os.path.splitext(fileName)[0]
             countFiles += 1
             with open(".tx/config", "a") as file:
                 file.write(f"[{project_slug}.{filePath}]\n")
-                file.write(f"file_filter = {subdir}/locales/<lang>\n")
+                file.write(f"file_filter = {subdir}/{fileNameNoExtension}.<lang>.md\n")
                 file.write(f"source_file = {fileRaw}\n")
                 file.write("source_lang = en\n")
                 file.write("type = GITHUBMARKDOWN\n")
                 file.write("\n")
+                
 print(f"Number of resources: {countFiles}")
