b5eea8748361373865522cb406a65ea77bc4331d
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2023-11-04 18:50:34 +0400

    Update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://nethermindeth.github.io',
+  url: 'https://docs.nethermind.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
