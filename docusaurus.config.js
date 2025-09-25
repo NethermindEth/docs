854d89a85c467ec16e8a445f91bba8baad8133a8
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-09-25 21:18:43 +0200

    Update packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,12 @@ const config = {
   projectName: 'docs',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks:{
+      onBrokenMarkdownLinks: 'warn'
+    }
+  },
 
   i18n: {
     defaultLocale: 'en',
